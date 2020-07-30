5981a7f61396e4e9a2d5d4fdb71d2f077f713345
claytoncc <clayton@openiot.co.za>
2020-07-30 12:53:54 +0200

    updates

--- dal/dal.js
+++ dal/dal.js
@@ -352,6 +352,8 @@ var module = function() {
 				"bitid.auth.users.email": 	args.req.body.header.email
 			};
 
+			console.log(JSON.stringify(params));
+
 			db.call({
 				'params': 		params,
 				'operation': 	'find',
