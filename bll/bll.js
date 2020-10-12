483acad6b09c8a6d5d49f86c998e600cbe7bb259
claytoncc <clayton@openiot.co.za>
2020-10-12 10:43:22 +0200

    updates

--- bll/bll.js
+++ bll/bll.js
@@ -194,9 +194,6 @@ var module = function () {
 									for (let i = 0; i < args.result.length; i++) {
 										if (i + 1 < args.result.length) {
 											let value = (args.result[i + 1].value - args.result[i].value);
-											// if (item.type == 'analog' && typeof(item.analog.offset) != 'undefined' && item.analog.offset !== null && item.analog.offset != '') {
-											// 	value = value + parseFloat(item.analog.offset);
-											// };
 											result.push({
 												'date': moment(args.result[i].date).format(format),
 												'value': value
@@ -208,9 +205,6 @@ var module = function () {
 								} else {
 									var item = args.result[args.result.length - 1];
 									item.value = args.result.map(o => {
-										// if (item.type == 'analog' && typeof(item.analog.offset) != 'undefined' && item.analog.offset !== null && item.analog.offset != '') {
-										// 	o.value = o.value + parseFloat(item.analog.offset);
-										// };
 										return {
 											'date': moment(o.date).format(format),
 											'value': o.value
@@ -236,11 +230,6 @@ var module = function () {
 											try {
 												var item = args.result[args.result.length - 1];
 												var result = [];
-												// args.result.map(o => {
-												// 	if (o.type == 'analog' && typeof(o.analog.offset) != 'undefined' && o.analog.offset !== null && o.analog.offset != '') {
-												// 		o.value = o.value + parseFloat(o.analog.offset);
-												// 	};
-												// });
 												for (let i = 0; i < args.result.length; i++) {
 													if (i + 1 < args.result.length) {
 														result.push(args.result[i + 1].value - args.result[i].value);
@@ -272,11 +261,6 @@ var module = function () {
 											};
 										} else {
 											var item = args.result[args.result.length - 1];
-											// args.result.map(o => {
-											// 	if (o.type == 'analog' && typeof(o.analog.offset) != 'undefined' && o.analog.offset !== null && o.analog.offset != '') {
-											// 		o.value = o.value + parseFloat(o.analog.offset);
-											// 	};
-											// });
 											var total = args.result.reduce((a, b) => a + b, 0);
 											var average = total / args.result.length;
 											item.value = parseFloat(average.toFixed(2));
